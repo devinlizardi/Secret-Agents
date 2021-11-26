1de9bc372cdffc8d7d78205a1c31ca28f8595efb
Devin Lizardi <dlizardi@berkeley.edu>
2021-11-26 10:18:25 -0800

    Spinning Cube

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('public/index.html', {root: __dirname});
+});
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+}); 
